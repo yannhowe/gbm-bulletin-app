dc75b32bca421d72877da718e3a6f45b6bf30cc6
Kwan Yann Howe <yannhowe@gmail.com>
2016-09-21 01:41:00 -0400

    added new url for bulletin pdf

--- web/newswire/urls.py
+++ web/newswire/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from . import views
-from .views import BulletinHomePageView, ProfileDetailFrontEndView, ProfileUpdateFrontEndView, RsvpUpdateView, RsvpListView, RsvpListViewRaw, AttendanceCreateFrontEnd, AnnouncementCreateFrontEnd
+from .views import BulletinHomePageView, ProfileDetailFrontEndView, ProfileUpdateFrontEndView, RsvpUpdateView, RsvpListView, RsvpListViewRaw, AttendanceCreateFrontEnd, AnnouncementCreateFrontEnd, BulletinPdfView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = (
@@ -13,10 +13,9 @@ urlpatterns = (
         login_required(ProfileUpdateFrontEndView.as_view()), name='profile_front_end_update'),
     url(r'^accounts/profile/$',
         login_required(ProfileDetailFrontEndView.as_view()), name='profile_front_end_detail'),
-
     url(r'^bulletin/submit/attendance/$',
         login_required(AttendanceCreateFrontEnd.as_view()), name='attendance_front_end_new'),
-
     url(r'^bulletin/submit/announcement/$',
         login_required(AnnouncementCreateFrontEnd.as_view()), name='announcement_front_end_new'),
+    url(r'^bulletin/pdf/$', BulletinPdfView.as_view(), name='bulletin_pdf'),
 )
